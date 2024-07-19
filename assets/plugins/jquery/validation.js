a4fe9d28e7481ad74ab5243ddaa519b3dda9c3fc
Venktesh Kale <121278886+venktesh-kale@users.noreply.github.com>
2024-07-19 22:32:43 +0530

    validation.js

--- assets/plugins/jquery/validation.js
+++ assets/plugins/jquery/validation.js
@@ -2,7 +2,7 @@
 function userlogin(form)/*function to check userid & password*/
 {
  /*the following code checkes whether the entered userid and password are matching*/
- if(form.userid.value == "venky@gmail.com" && form.usrpsw.value == "12345")
+ if(form.userid.value == "venky@gmail.com" && form.usrpsw.value == "1234")
   {
     window.open('index.html')/*opens the target page while Id & password matches*/
   }
